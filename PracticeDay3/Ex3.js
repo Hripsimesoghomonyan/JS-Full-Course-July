07c606304c9277b7382fe038f980eb41be07c836
Hripsimesoghomonyan <hripsime.soghomonyan95@gmail.com>
2023-08-19 14:23:57 +0400

    Task 3

--- PracticeDay3/Ex3.js
+++ PracticeDay3/Ex3.js
@@ -4,7 +4,6 @@ function findWordInMatrix(matrix, word) {
 
   for (let i = 0; i < n; i++) {
       for (let j = 0; j < m; j++) {
-          // Այստեղ մտածել լինի արդյունքը որպես false
           if (searchWord(matrix, word, i, j, 0)) {
               return true;
           }
@@ -15,7 +14,6 @@ function findWordInMatrix(matrix, word) {
 }
 
 function searchWord(matrix, word, row, col, index) {
-  // Կատարել սահմանված սահմանից դուրս չգալու
   if (
       row < 0 ||
       col < 0 ||
@@ -25,15 +23,15 @@ function searchWord(matrix, word, row, col, index) {
       return false;
   }
 
-  // Կատարել բառի գործողությունը
   if (matrix[row][col] === word[index]) {
       if (index === word.length - 1) {
-          return true; // Բառը գտնվել է
+          return true; 
       }
 
-      // Գտնել աջ, ձախ, վերը կամ ներքև
+
+      
       const temp = matrix[row][col];
-      matrix[row][col] = ''; // Խաչառեք եղած տառը, որպեսզի չեղարկի հաջորդական ուղղեցումը
+      matrix[row][col] = ''; 
       const found =
           searchWord(matrix, word, row + 1, col, index + 1) ||
           searchWord(matrix, word, row - 1, col, index + 1) ||
--- PracticeDay3/Ex6.js
+++ PracticeDay3/Ex6.js
@@ -2,19 +2,18 @@ function findLargestSubmatrix(matrix, k) {
   const n = matrix.length;
   let maxSubmatrixSize = 0;
 
-  // Գտնել ամենամեծ ներդրված kxk մատրիցայի երկարթյունը
+  
   for (let i = 0; i <= n - k; i++) {
       for (let j = 0; j <= n - k; j++) {
           let sum = 0;
 
-          // Հաշվել kxk մատրիցայի գումարը
           for (let row = i; row < i + k; row++) {
               for (let col = j; col < j + k; col++) {
                   sum += matrix[row][col];
               }
           }
 
-          // Թարմացնել ամենամեծ երկարթյունը
+          
           maxSubmatrixSize = Math.max(maxSubmatrixSize, sum);
       }
   }
@@ -22,7 +21,6 @@ function findLargestSubmatrix(matrix, k) {
   return maxSubmatrixSize;
 }
 
-// Մատրիցայի ներքևի օրինակ
 const matrix = [
   [0, 1, 0, 1, 0],
   [1, 1, 1, 1, 1],
