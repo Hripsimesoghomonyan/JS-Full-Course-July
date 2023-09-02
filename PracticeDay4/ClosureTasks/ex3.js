631d68a6dc82180d8f1f21831c120121b1beb131
Hripsimesoghomonyan <hripsime.soghomonyan95@gmail.com>
2023-09-02 14:00:44 +0400

    new

--- PracticeDay4/ClosureTasks/ex3.js
+++ PracticeDay4/ClosureTasks/ex3.js
@@ -1,22 +1,33 @@
-"use strict";
+// "use strict";
 
-function createCachingFunction() {
-  const cache = {};
+// function createCachingFunction() {
+//   const cache = {};
 
-  return function(input) {
-    if (input in cache) {
-      return `${cache[input]} (cached)`;
-    } 
-      const result = input * 2; 
-      cache[input] = result;
-      return result;
+//   return function(input) {
+//     if (input in cache) {
+//       return `${cache[input]} (cached)`;
+//     } 
+//       const result = input * 2; 
+//       cache[input] = result;
+//       return result;
     
-  };
+//   };
+// }
+
+// const cachedCalculation = createCachingFunction();
+
+// console.log(cachedCalculation(5));
+// console.log(cachedCalculation(5));
+// console.log(cachedCalculation(10));
+// console.log(cachedCalculation(5));
+class BankAccount {
+  static changeAccountType(accountType) {
+    this.newAccountType = accountType;
+    return this.newAccountType;
+  }
+  constructor({newAccountType = "Normal"} = {}) {
+    this.newAccountType = newAccountType;
+  }
 }
-
-const cachedCalculation = createCachingFunction();
-
-console.log(cachedCalculation(5));
-console.log(cachedCalculation(5));
-console.log(cachedCalculation(10));
-console.log(cachedCalculation(5));
+const cutomer = new BankAccount({newAccountType: "Gold"});
+console.log(cus);
