e1fe5318565812115a6f431c541b14e943f60275
HripsimeSoghomon <hripsime.soghomonyan95@gmail.com>
2023-09-16 13:16:45 +0400

    feat(ex3.js): create file and add code

---
+++ PracticeDay7/Errors/ex3.js
@@ -0,0 +1,17 @@
+"use strict"
+
+function reverseString(s) {
+  try {
+    if (typeof s !== "string") {
+      throw new Error("s.split is not a function");
+    } else {
+      s = s.split("").reverse().join("");
+      console.log(s);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+reverseString("Hello");
+reverseString(123);
